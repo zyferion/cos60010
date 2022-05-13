77825aa6af3ab6c012ea06f41973ed7028b340cc
Erika Raymundo <zyferion@gmail.com>
2022-05-13 22:13:29 +1000

    Update urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path
 
-# from . import views
+from .views import (
+    index,
+    QuizListView,
+    quiz_view,
+    quiz_data_view,
+    save_quiz_view
+)
 
 # Define app name
 app_name = 'quiz'
 
 # Register views here
 urlpatterns = [
-   # path('', views.index, name='index'),
-        
-    path('', views.QuizListView.as_view(), name='main-view'),
-    path('<pk>/', views.quiz_view, name='quiz-view'),
-    path('add_quiz/', views.add_quiz, name='add-quiz'),
-    path('add_quiz/addrecord', views.addrecord, name='addrecord'),
+    path('index/', index, name='index'),
+
+    path('', QuizListView.as_view(), name='main-view'),
+    path('<pk>/', quiz_view, name='quiz-view'),
+    path('<pk>/save/', save_quiz_view, name='save-view'),
+    path('<pk>/data/', quiz_data_view, name='quiz-data-view'),
 ]
