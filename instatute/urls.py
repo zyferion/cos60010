e3ac7a4afe5bd72aa2c43ca059dc0a9e4b091491
Andy King <80310189+kingandyj@users.noreply.github.com>
2022-05-27 11:56:27 +1000

    Formatting
    Run through autopep8

--- instatute/urls.py
+++ instatute/urls.py
@@ -31,18 +31,18 @@ urlpatterns = [
     # Quiz App
     path('', include('quiz.urls', namespace='quiz')),
 
-  # Users App
-    path('register/', user_views.register, name ='register'),
+    # Users App
+    path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
 
     # Subjects App
-   path('home/', subjects_views.home, name='home'),
-   
-   #Membership App
+    path('home/', subjects_views.home, name='home'),
+
+    # Membership App
     path('studentdetails/', membership_views.studentinfo, name='studentdetails')
 ]
 
-# Add static 
+# Add static
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
