9a8bf4a0b11f7251070534e9a6dafe4f4facfc31
Erika Raymundo <zyferion@gmail.com>
2022-05-27 15:32:14 +1000

    added dynamic numbers on dahsboard

--- subjects/views.py
+++ subjects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Subject, Enrolment
 from django.contrib.auth.decorators import login_required # Import login_required decorator
+from quiz.models import Result
+from django.db.models import Avg
 
 # Create your views here.
 @login_required # Require user logged in before they can access user home page
@@ -9,10 +11,25 @@ def home(request):
     user = request.user
 
     subjects = Subject.objects.all()
-
+    
     user_enrolments = Enrolment.objects.filter(user=user, is_active=1)
+    user_results = Result.objects.filter(user=user)
+
+    # count of user quiz attempts
+    quiz_count = user_results.count()
+
+    # count of user enrolments
+    enrolments_count = user_enrolments.count()
+
+    # average result score
+    avg_score = user_results.aggregate(Avg('score')).get('score__avg')
+    avg_score = round(avg_score,2)
 
     return render(request, 'subjects/home.html', 
     {   'subjects': subjects, 
-        'user_enrolments': user_enrolments
+        'user_enrolments': user_enrolments,
+        'user_results': user_results,
+        'quiz_count' : quiz_count,
+        'enrolments_count' : enrolments_count,
+        'avg_score': avg_score
         })
